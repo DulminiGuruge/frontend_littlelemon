4f9a23c5e4d3bb7c81bc0c3e84a6a8c1446ed04e
Dulmini Guruge <agg362@usask.ca>
2023-12-04 19:25:51 -0600

    Home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Main from './Components/Main';
+import Nav from './Components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <meta name="description" content="your text goes here"/>
+   <Header/>
+   <Nav/>
+   <Main/>
+   <Footer/>
+
+
+
+   </>
   );
 }
 
---
+++ src/Components/Nav.js
@@ -0,0 +1,17 @@
+function Nav(){
+
+    return(
+      
+    <nav>
+       
+        <ul>
+            <li><a href="final.html">HOME</a></li>
+            <li><a href="menu.html">MENU</a></li>
+            <li><a href="booking.html">BOOK</a></li>
+            <li><a href="about.html">ABOUT</a></li>
+        </ul>
+    </nav>
+    );
+
+}
+export default Nav;
